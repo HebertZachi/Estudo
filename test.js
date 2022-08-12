e2695e8e92748268ead9bdd290f6599d78490c82
Hebert Zachi <hebertzachi.contato@outlook.com>
2022-08-11 22:44:35 -0300

    Mesma coisa do projeto instagram

---
+++ test.js
@@ -0,0 +1,16 @@
+function comparaNumeros(num1, num2) {
+  if (num1 == num2) {
+    return console.log(`Os números ${num1} e ${num2}, são iguais`)
+  } else {
+    return console.log(`Os números ${num1} ${num2}, não são iguais`)
+  }
+}
+
+function ComparaSoma (num1, num2) {
+  if (num1 == num2) {
+    console.log(`Os números ${num1} e ${num2}, são iguais`)
+  } else {
+    console.log(`Os
+}
+
+const soma = ComparaSoma
